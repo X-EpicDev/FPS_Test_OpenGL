bed2f7bce6bf8a0e974e0324d60fc5a91c91643e
X-Epicness <masteraidansmith@gmail.com>
2024-08-31 19:43:56 +1000

    added GLAD and GLFW to the project to prepare for openGL usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main() {
-    std::cout << "Quick, Build Test" << std::endl;
+    std::cout << "GLAD and GLFW include paths are set correctly." << std::endl;
     return 0;
 }
